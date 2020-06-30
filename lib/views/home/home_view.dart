2cb9890f9c536aeb0f9882676096f57a745d9f9d
Siddharth Preetham <siddharthpreetham09@gmail.com>
2020-06-30 19:16:44 -0400

    added the background image for the stories holder

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,7 +1,6 @@
 import 'package:coldfiction/widgets/CenteredView/centered_view.dart';
 import 'package:coldfiction/widgets/HomeImage/home_image.dart';
 import 'package:coldfiction/widgets/NavigationBar/navigation_bar.dart';
-import 'package:coldfiction/widgets/Stories/stories.dart';
 import 'package:coldfiction/widgets/StoriesHolder/stories_holder.dart';
 import 'package:flutter/material.dart';
 //The homepage
