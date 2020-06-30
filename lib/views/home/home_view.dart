59ca930693b22dc2a1392a27db3b70c752f1e916
Siddharth Preetham <siddharthpreetham09@gmail.com>
2020-06-30 12:26:50 -0400

    Replaced the column widget with a listview widget to enable scrolling

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -3,7 +3,7 @@ import 'package:coldfiction/widgets/HomeImage/home_image.dart';
 import 'package:coldfiction/widgets/NavigationBar/navigation_bar.dart';
 import 'package:coldfiction/widgets/Stories/stories.dart';
 import 'package:flutter/material.dart';
-
+//The homepage
 class HomeView extends StatelessWidget {
   const HomeView({Key key}) : super(key: key);
 
@@ -13,10 +13,11 @@ class HomeView extends StatelessWidget {
         backgroundColor: Colors.transparent,
         body: Stack(
           children: <Widget>[
-            HomeImage(),
+            HomeImage(), //The background image
             CenteredView(
-              child: Column(
+              child: ListView(
                 children: <Widget>[
+                  Container(height: MediaQuery.of(context).size.height, width: MediaQuery.of(context).size.width, color: Colors.transparent,),
                   Stories(),
                 ],
               ),
