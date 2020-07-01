f9fbbf203cddb1e30fd90e0020e4c9cb774f3c39
Ananth Preetham <infinityp913@gmail.com>
2020-06-30 21:19:47 -0400

    added social media icons to assets, added inkwell response to navbar buttons, added social_bar.dart(incomplete)

---
+++ lib/widgets/Socials Bar/social_bar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class SocialBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: 
+      )
+    );
+  }
+}
